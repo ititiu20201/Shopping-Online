9a3cdc18b31c88540a6959bfbe4dc10433f077bd
Be <92241571+ititiu20201@users.noreply.github.com>
2024-06-17 10:13:03 +0700

    up

---
+++ Admin/src/assets/data.js
@@ -0,0 +1,34 @@
+import p1_img from './product_1.png'
+import p2_img from './product_2.png'
+import p3_img from './product_3.png'
+import p4_img from './product_4.png'
+
+let data_product = [
+  {
+    id:1,
+    name:"Adidas Originals Abstract Allover Animal Print short sleeve T-shirt",
+    image:p1_img,
+    new_price:50.00,
+    old_price:80.50,
+  },
+  {id:2,
+    name:"Adidas Originals Gothcore 3 Stripe T-shirt in purple",
+    image:p2_img,
+    new_price:85.00,
+    old_price:120.50,
+  },
+  {id:3,
+    name:"Adidas Originals Collar sweatshirt femmes blanc",
+    image:p3_img,
+    new_price:60.00,
+    old_price:100.50,
+  },
+  {id:4,
+    name:"White Adidas Originals Adibreak Hoodie",
+    image:p4_img,
+    new_price:100.00,
+    old_price:150.00,
+  },
+];
+
+export default data_product;
